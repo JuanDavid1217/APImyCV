9afd851f225bde9279565e17b03a819eb407ff5d
juan <juand_david2001@hotmail.com>
2023-10-17 12:17:33 -0600

    Created json file 3a_test3 #10

--- index.js
+++ index.js
@@ -49,6 +49,10 @@ app.get('/3a-test2', (req, res, next)=>{
 	res.sendfile("assets/3a_test2.json")
 })
 
+app.get('/3a-test3', (req, res, next)=>{
+	res.sendfile("assets/3a_test3.json")
+})
+
 app.listen(port,  () => 
 	console.log('listening on port ' + port
 ));
